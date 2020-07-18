6c2daf8f27574282b36205552b83f5334d43d68b
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-07-18 22:24:14 +0800

    run ok 1st time of pointnet
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

---
+++ main.py
@@ -0,0 +1,135 @@
+from ase.visualize import view
+from ase import Atoms, Atom
+from ase.db import connect
+from ase.calculators.morse import MorsePotential
+
+import random
+from ase.build import sort
+from ase import Atom
+from ase.io import read, write
+from base64 import b64encode, b64decode
+import itertools
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.utils.data
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+from pointnet import PointNetEncoder, PointNetReg
+
+
+seed = 1234
+random.seed(seed)
+np.random.seed(seed)
+
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+# properties
+A = "rotational_constant_A"
+B = "rotational_constant_B"
+C = "rotational_constant_C"
+mu = "dipole_moment"
+alpha = "isotropic_polarizability"
+homo = "homo"
+lumo = "lumo"
+gap = "gap"
+r2 = "electronic_spatial_extent"
+zpve = "zpve"
+U0 = "energy_U0"
+U = "energy_U"
+H = "enthalpy_H"
+G = "free_energy"
+Cv = "heat_capacity"
+
+db = connect('./qm9.db')
+# 16.1% memory usage of a 128G machine
+# rows = list(db.select(sort='id'))
+rows = list(db.select('id<200'))
+
+atom_names = ['H', 'C', 'O', 'F', 'N']
+atom_dict = {'H': 0, 'C':1, 'O':2, 'F':3, 'N':4}
+atom_cnt_lst = []
+atom_cnt_dict = {}
+
+model = PointNetReg()
+# train_lst = []
+# train_tgt = []
+
+def get_data_pp(idx, type):
+    # extract properties
+    prop = 'None'
+    # row = db.get(id=idx)
+    row = rows[idx-1]
+    if(row.id != idx):
+        1/0
+    # extract from schnetpack source code
+    shape = row.data["_shape_" + type]
+    dtype = row.data["_dtype_" + type]
+    prop = np.frombuffer(b64decode(row.data[type]), dtype=dtype)
+    prop = prop.reshape(shape)
+    return prop
+
+def gen_potential_data(data_count=100, atom_count=2, side_len=5):
+    input_lst = []
+    tgt_lst = []
+    for _ in range(data_count):
+        atom_coords = []
+        atoms = Atoms()
+        for _ in range(atom_count):
+            x = random.random() * side_len
+            y = random.random() * side_len
+            z = random.random() * side_len
+            atoms.append(Atom('Au', (x, y, z)))
+            atom_coords.append([x, y, z])
+        morse_calc = MorsePotential()
+        atoms.set_calculator(morse_calc)
+        engy = atoms.get_potential_energy()
+        
+        input_lst.append(atom_coords)
+        tgt_lst.append(engy)
+    return input_lst, tgt_lst
+
+if __name__ == '__main__':
+    # for row in rows:
+    #     xyz = row.toatoms().get_positions()
+    #     if train_lst.shape[0] == 0:
+    #         train_lst = np.array([xyz])
+    #         print(train_lst.shape)
+    #     else:
+    #         print(xyz.shape)
+    #         train_lst = np.dstack((train_lst, xyz))
+    #     train_tgt = np.append(train_tgt, get_data_pp(row.id, G))
+    #     # print(row.id, get_data_pp(row.id, G))
+    train_lst, train_tgt = gen_potential_data(data_count=1000, atom_count=2)
+    
+    criterion = torch.nn.MSELoss() # Defined loss function
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001) # Defined optimizer
+    x_data = torch.from_numpy(np.array(train_lst).transpose(0, 2, 1)).to('cpu')
+    y_data = torch.from_numpy(np.array(train_tgt)).to('cpu')
+
+    print(x_data.shape, y_data.shape)
+    for epoch in range(1000):
+        # Forward pass
+        y_pred = model(x_data)
+        print(y_pred.shape)
+    
+        # Compute loss
+        loss = criterion(y_pred, y_data)
+        '''
+        # Forward pass vali
+        y_pred_vali = model(x_data_vali.double())
+    
+        # Compute loss vali
+        loss_vali = criterion(y_pred_vali, y_data_vali)
+        '''
+        print(epoch, loss.item())
+
+        # Zero gradients
+        optimizer.zero_grad()
+        # perform backward pass
+        loss.backward()
+        # update weights
+        optimizer.step()
---
+++ pointnet.py
@@ -0,0 +1,139 @@
+'''
+Modify from pointnet pytorch implement:
+https://blog.csdn.net/weixin_39373480/article/details/88878629
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.utils.data
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class T_Net(nn.Module):
+    def __init__(self):
+        super(T_Net, self).__init__()
+        # 这里需要注意的是上文提到的MLP均由卷积结构完成
+        # 比如说将3维映射到64维，其利用64个1x3的卷积核
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3) # 输出为Batch*3*3的张量
+        return x
+
+class PointNetEncoder(nn.Module):
+    def __init__(self, global_feat = True):
+        super(PointNetEncoder, self).__init__()
+        self.stn = T_Net()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.global_feat = global_feat
+
+    def forward(self, x):
+        n_pts = x.size()[2]
+        trans = self.stn(x)
+        x = x.transpose(2,1)
+        x = torch.bmm(x, trans) # batch matrix multiply 即乘以T-Net的结果
+        x = x.transpose(2,1)
+        x = self.conv1(x)
+
+        x = F.relu(self.bn1(x))
+        pointfeat = x
+        x_skip = self.conv2(x)
+
+        x = F.relu(self.bn2(x_skip))
+        x = self.bn3(self.conv3(x))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024, 1)
+        if self.global_feat:
+            return x, trans
+        else:
+            x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)
+            return torch.cat([x, pointfeat], 1), trans
+
+class PointNetReg(nn.Module):
+    def __init__(self):
+        super(PointNetReg, self).__init__()
+        self.feat = PointNetEncoder(global_feat=True)
+        self.conv1 = torch.nn.Conv1d(1024, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, 1, 1)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        # print(x.size())
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans = self.feat(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        return x
+
+class PointNetCls(nn.Module):
+    def __init__(self, k = 2):
+        super(PointNetCls, self).__init__()
+        self.k = k
+        self.feat = PointNetEncoder(global_feat=False)
+        self.conv1 = torch.nn.Conv1d(1088, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans = self.feat(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        x = x.transpose(2,1).contiguous()
+        x = F.log_softmax(x.view(-1,self.k), dim=-1)
+        x = x.view(batchsize, n_pts, self.k)
+        return x
+
